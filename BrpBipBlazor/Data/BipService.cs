6189277768c7fc090fed2eddd389ac7e2e0c1c36
kry.ban <kry.ban@gmail.com>
2020-03-04 22:05:06 +0100

    Introducing BipService instead using BrpBipApi

---
+++ BrpBipBlazor/Data/BipService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BrpBipApi.Controllers;
+using BrpBipApi.Types;
+
+namespace BrpBipBlazor.Data
+{
+    public class BipService
+    {
+        IngeschrevenPersoonController bipApi = new IngeschrevenPersoonController();
+
+        public Persoon GetPersoon(int id = 999993653)
+        {
+            return bipApi.GetPersoon(id.ToString());
+        }
+    }
+}
