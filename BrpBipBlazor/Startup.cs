a073877c188cf6a810e406830d429393a93cb6d6
kry.ban <kry.ban@gmail.com>
2020-03-05 22:04:40 +0100

    Completed the line from service toblazor component.

--- BrpBipBlazor/Startup.cs
+++ BrpBipBlazor/Startup.cs
@@ -29,6 +29,8 @@ namespace BrpBipBlazor
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<BipService>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
