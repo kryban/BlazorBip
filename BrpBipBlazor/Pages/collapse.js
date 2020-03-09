8c69b00bdd791407257bbd74d92cb62dc15b8294
kry.ban <kry.ban@gmail.com>
2020-03-09 22:39:45 +0100

    Tryings to call javascript (collapse) method form blazor component

---
+++ BrpBipBlazor/Pages/collapse.js
@@ -0,0 +1,10 @@
+﻿$(".head").click(function () {
+    if ($("#collapsable").hasClass("out")) {
+        $("#collapsable").addClass("in");
+        $("#collapsable").removeClass("out");
+    }
+    else {
+        $("#collapsable").addClass("out");
+        $("#collapsable").removeClass("in");
+    }
+});
